e7e4ac78fcfd5489ac15ad890fe55de62859a287
YashMaini30 <mainiyash2@gmail.com>
2024-06-27 18:49:05 +0530

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -13,6 +13,15 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors:{
+      background : 'rgba(255, 255, 255, 0.8)',
+      shadow1: '0px 7px 14px rgba(0, 0, 0, .05), 0px 0px 3.12708px rgba(0, 0, 0, .0798), 0px 0px .931014px rgba(0, 0, 0, .1702)',
+      shadow2: '0 0 0 1px #4a47b1',
+      border1: '#b8b8b8',
+      border2: '#d6d6d6',
+      text1: '#3d3d3d',
+      text2: '#e3e2fe'
+    }
   },
   plugins: [],
 };
